5d6757bca4ee38429174636b02f44af7fe77ada8
theopakieser <20101918@mail.wit.ie>
2026-04-14 17:36:54 +0100

    add quest model for campaign map markers

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,6 +92,8 @@ dependencies {
     implementation(libs.play.services.auth)
     implementation(libs.googleid)
 
+    implementation(libs.maps.compose)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ app/src/main/java/ie/setu/questledger/models/QuestModel.kt
@@ -0,0 +1,11 @@
+package ie.setu.questledger.models
+
+data class QuestModel(
+    val id: String = "",
+    val email: String = "",
+    val title: String = "",
+    val description: String = "",
+    val status: String = "Active",
+    val latitude: Double = 0.0,
+    val longitude: Double = 0.0
+)
